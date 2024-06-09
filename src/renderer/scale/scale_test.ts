8e7399e2c32e2db57d37e818446ddc0ccbb19f7c
Joe Gregorio <joe@bitworking.org>
2024-06-09 11:39:19 -0400

    more scale tests

--- src/renderer/scale/scale_test.ts
+++ src/renderer/scale/scale_test.ts
@@ -2,23 +2,40 @@ import { assertEquals } from "https://deno.land/std/assert/mod.ts";
 import { RenderOptions } from "../renderer.ts";
 import { Coordiate, Feature, Point, Scale } from "./scale.ts";
 
-Deno.test("Basic size calculations:", () => {
-  const opts: RenderOptions = {
-    fontSizePx: 12,
-    hasText: true,
-    displaySubRange: null,
-    colorTheme: {
-      surface: "#000",
-      onSurface: "#222",
-    },
-    marginSizePx: 10,
-  };
-  const s = new Scale(opts, 256, 0, 20, 10);
+const optsForTest: RenderOptions = {
+  fontSizePx: 12,
+  hasText: true,
+  displaySubRange: null,
+  colorTheme: {
+    surface: "#000",
+    onSurface: "#222",
+  },
+  marginSizePx: 10,
+};
+
+Deno.test("Basic size calculations 12px font:", () => {
+  optsForTest.fontSizePx = 12;
+  const s = new Scale(optsForTest, 256, 0, 20, 10);
   assertEquals(s.feature(1, 0, Feature.percentHeight), 1);
   assertEquals(s.feature(1, 0, Feature.taskLineHeight), 3);
 
   assertEquals(s["dayWidthPx"], 11);
   assertEquals(s["blockSizePx"], 4);
+  assertEquals(
+    s.coord(1, 1, Coordiate.taskLineStart),
+    // margin + dayWidthPx, margin + rowHeight + 5*blockSize
+    new Point(10 + 11, 10 + 6 * 4 + 5 * 4)
+  );
+});
+
+Deno.test("Basic size calculations 24px font:", () => {
+  optsForTest.fontSizePx = 24;
+  const s = new Scale(optsForTest, 256, 0, 20, 10);
+  assertEquals(s.feature(1, 0, Feature.percentHeight), 3);
+  assertEquals(s.feature(1, 0, Feature.taskLineHeight), 7);
+
+  assertEquals(s["dayWidthPx"], 22);
+  assertEquals(s["blockSizePx"], 8);
   assertEquals(
     s.coord(1, 1, Coordiate.taskLineStart),
     // margin + dayWidthPx, margin + rowHeight + 5*blockSize
