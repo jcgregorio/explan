5c20243323ed2464eadd63bb63e0fa57d561c9fb
Joe Gregorio <joe@bitworking.org>
2024-10-06 15:25:15 -0400

    Tests for Plan serialization to/from JSON.

--- src/plan/plan_test.ts
+++ src/plan/plan_test.ts
@@ -1,5 +1,13 @@
 import { assert } from "@esm-bundle/chai";
 import { Plan, PlanSerialized } from "./plan";
+import {
+  AddResourceOp,
+  AddResourceOptionOp,
+  SetResourceValueOp,
+} from "../ops/resources";
+import { Op, applyAllOpsToPlan } from "../ops/ops";
+import { SetMetricValueOp } from "../ops/metrics";
+import { InsertNewEmptyTaskAfterOp, SetTaskNameOp } from "../ops/chart";
 
 describe("Plan", () => {
   it("Round trips via JSON", () => {
@@ -9,5 +17,48 @@ describe("Plan", () => {
       JSON.parse(serialized) as PlanSerialized
     );
     assert.equal(serialized, JSON.stringify(deserialized, null, "  "));
+  });
+
+  it("Round trips via JSON with Tasks and Edges", () => {
+    // Create a Plan with a task and resource and metric to confirm that they
+    // all roundtrip correctly.
+    const people: string[] = ["Fred", "Barney", "Wilma", "Betty"];
+    const ops: Op[] = [AddResourceOp("Person")];
+    people.forEach((person: string) => {
+      ops.push(AddResourceOptionOp("Person", person));
+    });
+
+    ops.push(
+      InsertNewEmptyTaskAfterOp(0),
+      SetMetricValueOp("Duration", 10, 1),
+      SetTaskNameOp(1, "MyFirstTask"),
+      SetResourceValueOp("Person", people[1], 1),
+      SetResourceValueOp("Uncertainty", "moderate", 1)
+    );
+
+    const ret = applyAllOpsToPlan(ops, new Plan());
+    assert.isTrue(ret.ok);
+    const plan = ret.value.plan;
+
+    // Now serialize it.
+    const serialized = JSON.stringify(plan, null, "  ");
+
+    // Now reconstitute from that serialization.
+    const deserialized = new Plan().fromJSON(
+      JSON.parse(serialized) as PlanSerialized
+    );
+
+    // Confirm they are equivalent.
+    assert.equal(serialized, JSON.stringify(deserialized, null, "  "));
+    assert.equal(deserialized.chart.Vertices[1].name, "MyFirstTask");
+    assert.equal(deserialized.chart.Vertices[1].getMetric("Duration"), 10);
+    assert.equal(
+      deserialized.chart.Vertices[1].getResource("Person"),
+      people[1]
+    );
+    assert.equal(
+      deserialized.chart.Vertices[1].getResource("Uncertainty"),
+      "moderate"
+    );
   });
 });
