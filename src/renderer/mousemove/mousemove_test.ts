e549ccd9654165927bfac6f57b583ba3eb8b5e82
Joe Gregorio <joe@bitworking.org>
2024-11-16 10:37:14 -0500

    Add tests for MouseMove.

--- src/renderer/mousemove/mousemove_test.ts
+++ src/renderer/mousemove/mousemove_test.ts
@@ -21,7 +21,13 @@ describe("MouseMove", () => {
   });
 
   it("Returns a new point if it moved since the last read. ", () => {
+    // Note we send clientX and clientY, but read offsetX and offsetY, so we
+    // won't know the actual mouse coordinates returned.
     div.dispatchEvent(new MouseEvent("mousemove", { clientX: 1, clientY: 2 }));
-    assert.deepEqual(mm.readLocation(), new Point(1, 2));
+    assert.isNotNull(mm.readLocation());
+
+    // And check that reading had reset the lastReadLocation and we now return
+    // null until the mouse moves again.
+    assert.isNull(mm.readLocation());
   });
 });
