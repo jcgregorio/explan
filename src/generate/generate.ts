5d507b2cab296ba806c84ab84d883ebf5b6dbfc3
Joe Gregorio <joe@bitworking.org>
2024-12-15 16:10:55 -0500

    Use Lorem Ipsum to generate the random task names.

--- src/generate/generate.ts
+++ src/generate/generate.ts
@@ -29,8 +29,6 @@ export const generateRandomPlan = (): Plan => {
   const plan = new Plan();
   let taskID = 0;
 
-  const rndName = (): string => `T ${taskID++}`;
-
   const ops: Op[] = [AddResourceOp("Person")];
 
   people.forEach((person: string) => {
@@ -40,7 +38,7 @@ export const generateRandomPlan = (): Plan => {
   ops.push(
     InsertNewEmptyTaskAfterOp(0),
     SetMetricValueOp("Duration", rndDuration(), 1),
-    SetTaskNameOp(1, rndName()),
+    SetTaskNameOp(1, randomTaskName()),
     SetResourceValueOp("Person", people[rndInt(people.length)], 1),
     SetResourceValueOp("Uncertainty", "moderate", 1)
   );
@@ -51,7 +49,7 @@ export const generateRandomPlan = (): Plan => {
     ops.push(
       SplitTaskOp(index),
       SetMetricValueOp("Duration", rndDuration(), index + 1),
-      SetTaskNameOp(index + 1, rndName()),
+      SetTaskNameOp(index + 1, randomTaskName()),
       SetResourceValueOp("Person", people[rndInt(people.length)], index + 1),
       SetResourceValueOp("Uncertainty", "moderate", index + 1)
     );
@@ -60,7 +58,7 @@ export const generateRandomPlan = (): Plan => {
     ops.push(
       DupTaskOp(index),
       SetMetricValueOp("Duration", rndDuration(), index + 1),
-      SetTaskNameOp(index + 1, rndName()),
+      SetTaskNameOp(index + 1, randomTaskName()),
       SetResourceValueOp("Person", people[rndInt(people.length)], index + 1),
       SetResourceValueOp("Uncertainty", "moderate", index + 1)
     );
@@ -74,3 +72,80 @@ export const generateRandomPlan = (): Plan => {
   }
   return plan;
 };
+
+const parts = [
+  "lorem",
+  "ipsum",
+  "dolor",
+  "sit",
+  "amet",
+  "consectetur",
+  "adipiscing",
+  "elit",
+  "sed",
+  "do",
+  "eiusmod",
+  "tempor",
+  "incididunt",
+  "ut",
+  "labore",
+  "et",
+  "dolore",
+  "magna",
+  "aliqua",
+  "ut",
+  "enim",
+  "ad",
+  "minim",
+  "veniam",
+  "quis",
+  "nostrud",
+  "exercitation",
+  "ullamco",
+  "laboris",
+  "nisi",
+  "ut",
+  "aliquip",
+  "ex",
+  "ea",
+  "commodo",
+  "consequat",
+  "euis",
+  "aute",
+  "irure",
+  "dolor",
+  "in",
+  "reprehenderit",
+  "in",
+  "voluptate",
+  "velit",
+  "esse",
+  "cillum",
+  "dolore",
+  "eu",
+  "fugiat",
+  "nulla",
+  "pariatur",
+  "excepteur",
+  "sint",
+  "occaecat",
+  "cupidatat",
+  "non",
+  "proident",
+  "sunt",
+  "in",
+  "culpa",
+  "qui",
+  "officia",
+  "deserunt",
+  "mollit",
+  "anim",
+  "id",
+  "est",
+  "laborum",
+];
+
+const partsLength = parts.length;
+
+const randomTaskName = (): string =>
+  `${parts[rndInt(partsLength)]} ${parts[rndInt(partsLength)]}`;
