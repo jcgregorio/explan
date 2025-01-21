dfca25ca868a4350420db6621388357bde2183c3
Joe Gregorio <joe@bitworking.org>
2025-01-20 23:03:49 -0500

    More tests for parseDuration.

--- src/units/parse.ts
+++ src/units/parse.ts
@@ -1,13 +1,17 @@
+import { Result, error, ok } from "../result";
+
 const decimalRegex = /^[\d\.]+$/;
 const shorthandRegex = /^(\d+d)?(\d+w)?(\d+m)?(\d+y)?$/;
 
-export const parseDuration = (s: string, daysInWeek: 5 | 7): number => {
+export const parseDuration = (s: string, daysInWeek: 5 | 7): Result<number> => {
   if (s.match(decimalRegex)) {
-    return +s;
+    return ok(+s);
   }
   let ret = 0;
   let num = 0;
-  [...s].forEach((c: string) => {
+  const chars = [...s];
+  for (let i = 0; i < chars.length; i++) {
+    const c = chars[i];
     if (c === "d") {
       ret += num;
       num = 0;
@@ -20,8 +24,8 @@ export const parseDuration = (s: string, daysInWeek: 5 | 7): number => {
     } else if ("0123456789".includes(c)) {
       num = num * 10 + +c;
     } else {
-      // Error?!?!
+      return error(new Error(`invalid duration format: ${s}`));
     }
-  });
-  return ret;
+  }
+  return ok(ret);
 };
--- src/units/parse_test.ts
+++ src/units/parse_test.ts
@@ -1,15 +1,32 @@
 import { assert } from "@esm-bundle/chai";
 import { parseDuration } from "./parse.ts";
+import { Result } from "../result.ts";
+
+const isOK = (r: Result<number>): number => {
+  assert.isTrue(r.ok);
+  return r.value;
+};
 
 describe("parseDuration", () => {
   it("handles shorthand durations", () => {
-    assert.equal(parseDuration("1w", 7), 7);
-    assert.equal(parseDuration("1w", 5), 5);
-    assert.equal(parseDuration("1d1w", 5), 6);
-    assert.equal(parseDuration("1w1d", 5), 6);
-    assert.equal(parseDuration("1w1d1m", 5), 4 * 5 + 5 + 1);
-    assert.equal(parseDuration("1w1d1m", 7), 4 * 7 + 7 + 1);
-    assert.equal(parseDuration("10d", 7), 10);
-    assert.equal(parseDuration("10d", 7), 10);
+    assert.equal(isOK(parseDuration("1w", 7)), 7);
+    assert.equal(isOK(parseDuration("1w", 5)), 5);
+    assert.equal(isOK(parseDuration("1d1w", 5)), 6);
+    assert.equal(isOK(parseDuration("1w1d", 5)), 6);
+    assert.equal(isOK(parseDuration("1w1d1m", 5)), 4 * 5 + 5 + 1);
+    assert.equal(isOK(parseDuration("1w1d1m", 7)), 4 * 7 + 7 + 1);
+    assert.equal(isOK(parseDuration("10d", 7)), 10);
+    assert.equal(isOK(parseDuration("10d", 7)), 10);
+  });
+
+  it("handles numbers", () => {
+    assert.equal(isOK(parseDuration("1", 5)), 1);
+    assert.equal(isOK(parseDuration("100", 5)), 100);
+    assert.equal(isOK(parseDuration("1101.124", 5)), 1101.124);
+  });
+
+  it("detects invalid durations numbers", () => {
+    assert.isFalse(parseDuration("f", 5).ok);
+    assert.isFalse(parseDuration("123z", 5).ok);
   });
 });
