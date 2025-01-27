57a6022a2661cd91bab6fd8dda2e46f022080890
Joe Gregorio <joe@bitworking.org>
2025-01-26 20:17:33 -0500

    Add WeekDays tests.

--- src/units/unit_test.ts
+++ src/units/unit_test.ts
@@ -2,18 +2,19 @@ import { assert } from "@esm-bundle/chai";
 import { MetricDefinition } from "../metrics/metrics";
 import { MetricRange } from "../metrics/range";
 import { Precision } from "../precision/precision";
-import { Days } from "./unit.ts";
+import { Days, WeekDays } from "./unit.ts";
 import { Result } from "../result.ts";
 
 describe("Units", () => {
+  const m = new MetricDefinition(
+    0,
+    new MetricRange(1),
+    false,
+    new Precision(2)
+  );
+  const start = new Date("2025-01-22T12:00:00");
+
   describe("Days", () => {
-    const m = new MetricDefinition(
-      0,
-      new MetricRange(1),
-      false,
-      new Precision(2)
-    );
-    const start = new Date("2025-01-22T12:00:00");
     const d = new Days(start, m);
 
     const isOK = (r: Result<number>) => {
@@ -42,6 +43,48 @@ describe("Units", () => {
       assert.equal(isOK(d.parse("2d1w")), 9);
       assert.equal(isOK(d.parse("2d3m")), 86);
       assert.equal(isOK(d.parse("1w2d3m")), 93);
+      assert.equal(
+        isOK(d.parse("w")),
+        1,
+        " w parses as 0w which is 0, but is clamped to 1."
+      );
+    });
+
+    it("displays durations correctly", () => {
+      assert.equal(d.displayTime(0, "en-US"), "1/22/2025");
+      assert.equal(d.displayTime(1.2, "en-US"), "1/23/2025");
+    });
+  });
+
+  describe("WeekDays", () => {
+    const d = new WeekDays(start, m);
+
+    const isOK = (r: Result<number>) => {
+      assert.isTrue(r.ok);
+      return r.value;
+    };
+
+    it("returns error on bad input", () => {
+      assert.isFalse(d.parse("f").ok);
+    });
+
+    it("clamps input to the range", () => {
+      assert.equal(isOK(d.parse("0")), 1, "clamped from 0 to 1");
+    });
+
+    it("rounds to the precision", () => {
+      assert.equal(
+        isOK(d.parse("1.123456")),
+        1.12,
+        "rounded to precision of 2"
+      );
+    });
+
+    it("understands duration shorthands", () => {
+      assert.equal(isOK(d.parse("1w")), 5);
+      assert.equal(isOK(d.parse("2d1w")), 7);
+      assert.equal(isOK(d.parse("2d3m")), 2 + 3 * 4 * 5);
+      assert.equal(isOK(d.parse("1w2d3m")), 5 + 2 + 3 * 4 * 5);
       assert.equal(
         isOK(d.parse("w")),
         1,
