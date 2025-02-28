51095ab90fad9b8634916eda7e5c4489def83f5b
Joe Gregorio <joe@bitworking.org>
2025-02-28 14:13:00 -0500

    Adding PlanStatus as it's own type/file.

---
+++ src/plan_status/plan_status.ts
@@ -0,0 +1,42 @@
+import { Plan } from "../plan/plan";
+
+export type PlanStatus =
+  | { stage: "unstarted" }
+  | {
+      stage: "started";
+      start: Date;
+    };
+
+export type PlanStatusSerialized = {
+  stage: string;
+  start: number;
+};
+
+export const toJSON = (p: PlanStatus): PlanStatusSerialized => {
+  const ret: PlanStatusSerialized = {
+    stage: "unstarted",
+    start: 0,
+  };
+  if (p.stage === "started") {
+    ret.start = p.start.valueOf();
+  }
+  return ret;
+};
+
+export const fromJSON = (p: PlanStatusSerialized) : PlanStatus => {
+    const unstarted : PlanStatus = {stage: "unstarted"};
+
+    if (p.stage === undefined) {
+        return unstarted;
+    }
+    if (p.stage === "started") {
+        if (p.start === undefined) {
+            return unstarted
+        }
+        return {
+            stage: "started",
+            start: new Date(p.start);
+        }
+    }
+    return unstarted
+}
