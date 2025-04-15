26f4c5da4648004b3b39e0aa3a9b40ff40147071
Joe Gregorio <joe@bitworking.org>
2025-04-15 09:45:21 -0400

    Async web test.

--- src/image/image_test.ts
+++ src/image/image_test.ts
@@ -1,4 +1,4 @@
-import { PngMetadata } from '../vendor/png-metadata/src/png-metadata';
+import { Chunk, PngMetadata } from '../vendor/png-metadata/src/png-metadata';
 import { assert } from '@esm-bundle/chai';
 
 describe('PngMetadata', () => {
@@ -6,23 +6,39 @@ describe('PngMetadata', () => {
     assert.isFalse(PngMetadata.isPNG(new Uint8Array()));
   });
 
-  it('can determine if a file in a PNG', (done) => {
-    const img = document.createElement('img');
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = img.width;
-      canvas.height = img.height;
+  const loadImageAsBlob = () =>
+    new Promise<Blob>(function (resolve) {
+      const img = document.createElement('img');
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-      const ctx = canvas.getContext('2d')!;
-      ctx.drawImage(img, 0, 0);
-      canvas.toBlob(async (blob: Blob | null) => {
-        assert.isNotNull(blob);
-        const bin = new Uint8Array(await blob.arrayBuffer());
-        assert.isTrue(PngMetadata.isPNG(bin));
-        done();
-      });
-    };
-    img.src = './src/image/testdata/simple.png';
-    document.body.appendChild(img);
+        const ctx = canvas.getContext('2d')!;
+        ctx.drawImage(img, 0, 0);
+
+        canvas.toBlob(async (blob: Blob | null) => {
+          assert.isNotNull(blob);
+          resolve(blob);
+        });
+      };
+      img.src = './src/image/testdata/simple.png';
+      document.body.appendChild(img);
+    });
+
+  it('can determine if a file in a PNG', async () => {
+    const blob = await loadImageAsBlob();
+    const bin = new Uint8Array(await blob.arrayBuffer());
+    assert.isTrue(PngMetadata.isPNG(bin));
+  });
+
+  it('can read chunks of a PNG', async () => {
+    const blob = await loadImageAsBlob();
+    const bin = new Uint8Array(await blob.arrayBuffer());
+    const chunks = PngMetadata.splitChunks(bin);
+    assert.deepEqual(
+      chunks.map((chunk: Chunk) => chunk.type),
+      ['IHDR', 'sRGB', 'IDAT', 'IEND']
+    );
   });
 });
