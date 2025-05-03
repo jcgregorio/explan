5484e2780ae74e759537aa96471684917363f5d5
Joe Gregorio <joe@bitworking.org>
2025-05-03 10:49:26 -0400

    Complete parse.ts to convert to/from human descriptions of duration.

--- src/units/parse.ts
+++ src/units/parse.ts
@@ -2,10 +2,38 @@ import { Result, error, ok } from '../result';
 
 const decimalRegex = /^[\d\.]+$/;
 
-export const parseDuration = (s: string, daysInWeek: 5 | 7): Result<number> => {
+/**
+ * Parses a human duration, such as "1w3d" to a number of days.
+ *
+ * The actualy number of days returned depends on the units of what a day means,
+ * which is passed in via daysInWeek.
+ *
+ * The values of daysInWeek mean:
+ *
+ *   7 - 7 days in a week (weekends aren't special).
+ *
+ *   5 - 5 days a week (don't count weekends).
+ *
+ *   0 - This is a unitless duration, not days, values that aren't just numbers,
+ *       such as '1w2d' will return an error.
+ */
+export const parseHumanDuration = (
+  s: string,
+  daysInWeek: 0 | 5 | 7
+): Result<number> => {
   s = s.trim();
+
+  // Empty string is always 0.
+  if (s === '') {
+    return ok(0);
+  }
+
   if (s.match(decimalRegex)) {
     return ok(+s);
+  }
+
+  if (daysInWeek === 0) {
+    return error(new Error(`${s} is not a valid unitless duration.`));
   }
   let ret = 0;
   let num = 0;
@@ -31,4 +59,56 @@ export const parseDuration = (s: string, daysInWeek: 5 | 7): Result<number> => {
     }
   }
   return ok(ret);
+};
+
+/**
+ * Converts a number duration into a human duration string.
+ *
+ *
+ * The values of daysInWeek mean:
+ *
+ *   7 - 7 days in a week (weekends aren't special). E: 8 -> "1w1d"
+ *
+ *   5 - 5 days a week (don't count weekends). E: 8 -> "1w3d"
+ *
+ *   0 - This is a unitless duration, not days, values that aren't just numbers,
+ *       such as '1w2d' will return an error. Ex: 8 -> "8".
+ *
+ */
+export const durationToHuman = (
+  days: number,
+  daysInWeek: 0 | 5 | 7
+): Result<string> => {
+  if (days < 0) {
+    return error(new Error(`Can't convert negative days: ${days}`));
+  }
+  if (days === 0) {
+    return ok('0');
+  }
+  if (daysInWeek === 0) {
+    return ok(`${days}`);
+  }
+  days = Math.floor(days);
+  const daysInYear = 52 * daysInWeek;
+  const daysInMonth = 4 * daysInWeek + 2;
+  const parts: string[] = [];
+  if (days >= daysInYear) {
+    const years = Math.floor(days / daysInYear);
+    days = days % daysInYear;
+    parts.push(`${years}y`);
+  }
+  if (days >= daysInMonth) {
+    const months = Math.floor(days / daysInMonth);
+    days = days % daysInMonth;
+    parts.push(`${months}m`);
+  }
+  if (days >= daysInWeek) {
+    const weeks = Math.floor(days / daysInWeek);
+    days = days % daysInWeek;
+    parts.push(`${weeks}w`);
+  }
+  if (days > 0) {
+    parts.push(`${days}d`);
+  }
+  return ok(parts.join(''));
 };
--- src/units/parse_test.ts
+++ src/units/parse_test.ts
@@ -1,38 +1,79 @@
 import { assert } from '@esm-bundle/chai';
-import { parseDuration } from './parse.ts';
+import { durationToHuman, parseHumanDuration } from './parse.ts';
 import { Result } from '../result.ts';
 
-const isOK = (r: Result<number>): number => {
+const isOK = <T>(r: Result<T>): T => {
   assert.isTrue(r.ok);
   return r.value;
 };
 
 describe('parseDuration', () => {
   it('handles shorthand durations', () => {
-    assert.equal(isOK(parseDuration('w', 7)), 0);
-    assert.equal(isOK(parseDuration('1w', 7)), 7);
-    assert.equal(isOK(parseDuration('1w', 5)), 5);
-    assert.equal(isOK(parseDuration('1d1w', 5)), 6);
-    assert.equal(isOK(parseDuration('1w1d', 5)), 6);
-    assert.equal(isOK(parseDuration('1w1d1m', 5)), 4 * 5 + 5 + 3);
-    assert.equal(isOK(parseDuration('1w1d1m', 7)), 4 * 7 + 7 + 3);
-    assert.equal(isOK(parseDuration('10d', 7)), 10);
-    assert.equal(isOK(parseDuration('10d', 7)), 10);
-    assert.equal(isOK(parseDuration('1y', 7)), 364);
-    assert.equal(isOK(parseDuration('1y', 5)), 260);
-    assert.equal(isOK(parseDuration('', 5)), 0);
-    assert.equal(isOK(parseDuration('  \n', 5)), 0);
+    assert.equal(isOK(parseHumanDuration('w', 7)), 0);
+    assert.equal(isOK(parseHumanDuration('1w', 7)), 7);
+    assert.equal(isOK(parseHumanDuration('1w', 5)), 5);
+    assert.equal(isOK(parseHumanDuration('1d1w', 5)), 6);
+    assert.equal(isOK(parseHumanDuration('1w1d', 5)), 6);
+    assert.equal(isOK(parseHumanDuration('1w1d1m', 5)), 4 * 5 + 5 + 3);
+    assert.equal(isOK(parseHumanDuration('1w1d1m', 7)), 4 * 7 + 7 + 3);
+    assert.equal(isOK(parseHumanDuration('10d', 7)), 10);
+    assert.equal(isOK(parseHumanDuration('10d', 7)), 10);
+    assert.equal(isOK(parseHumanDuration('1y', 7)), 364);
+    assert.equal(isOK(parseHumanDuration('1y', 5)), 260);
+    assert.equal(isOK(parseHumanDuration('', 5)), 0);
+    assert.equal(isOK(parseHumanDuration('  \n', 5)), 0);
+
+    assert.equal(isOK(parseHumanDuration('  \n', 0)), 0);
+    assert.equal(isOK(parseHumanDuration('5', 0)), 5);
+    assert.equal(isOK(parseHumanDuration('07.500', 0)), 7.5);
   });
 
   it('handles numbers', () => {
-    assert.equal(isOK(parseDuration('1', 5)), 1);
-    assert.equal(isOK(parseDuration('100', 5)), 100);
-    assert.equal(isOK(parseDuration('1101.124', 5)), 1101.124);
+    assert.equal(isOK(parseHumanDuration('1', 5)), 1);
+    assert.equal(isOK(parseHumanDuration('100', 5)), 100);
+    assert.equal(isOK(parseHumanDuration('1101.124', 5)), 1101.124);
   });
 
   it('detects invalid durations numbers', () => {
-    assert.isFalse(parseDuration('f', 5).ok);
-    assert.isFalse(parseDuration('123z', 5).ok);
-    assert.isFalse(parseDuration('-12', 5).ok);
+    assert.isFalse(parseHumanDuration('f', 5).ok);
+    assert.isFalse(parseHumanDuration('123z', 5).ok);
+    assert.isFalse(parseHumanDuration('-12', 5).ok);
+
+    assert.isFalse(parseHumanDuration('f', 7).ok);
+    assert.isFalse(parseHumanDuration('123z', 7).ok);
+    assert.isFalse(parseHumanDuration('-12', 7).ok);
+
+    assert.isFalse(parseHumanDuration('w', 0).ok);
+    assert.isFalse(parseHumanDuration('2m', 0).ok);
+  });
+});
+
+describe('durationToHuman', () => {
+  it('converts correctly to human duration strings', () => {
+    assert.equal(isOK(durationToHuman(7.5, 0)), '7.5');
+    assert.equal(isOK(durationToHuman(7.5, 5)), '1w2d');
+    assert.equal(isOK(durationToHuman(7.5, 7)), '1w');
+
+    assert.equal(isOK(durationToHuman(0, 0)), '0');
+    assert.equal(isOK(durationToHuman(0, 5)), '0');
+    assert.equal(isOK(durationToHuman(0, 7)), '0');
+
+    assert.equal(isOK(durationToHuman(1, 0)), '1');
+    assert.equal(isOK(durationToHuman(1, 5)), '1d');
+    assert.equal(isOK(durationToHuman(1, 7)), '1d');
+
+    assert.equal(isOK(durationToHuman(8, 0)), '8');
+    assert.equal(isOK(durationToHuman(8, 5)), '1w3d');
+    assert.equal(isOK(durationToHuman(8, 7)), '1w1d');
+
+    assert.equal(isOK(durationToHuman(30, 0)), '30');
+    assert.equal(isOK(durationToHuman(30, 5)), '1m1w3d');
+    assert.equal(isOK(durationToHuman(4 * 5 + 2, 5)), '1m');
+    assert.equal(isOK(durationToHuman(30, 7)), '1m');
+
+    assert.equal(isOK(durationToHuman(365, 0)), '365');
+    assert.equal(isOK(durationToHuman(365, 5)), '1y4m3w2d');
+    assert.equal(isOK(durationToHuman(5 * 52, 5)), '1y');
+    assert.equal(isOK(durationToHuman(365, 7)), '1y1d');
   });
 });
