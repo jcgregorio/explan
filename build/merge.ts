e500d0b36de55d00aa23d9a95f0e3ac5ccb6d8f5
Joe Gregorio <joe@bitworking.org>
2025-05-10 21:41:45 -0400

    Restructure the build so we always build dev and prod versions of both the standalone application and the extension.

--- build/merge.ts
+++ build/merge.ts
@@ -1,20 +1,41 @@
 // Script that merges the HTML file and the JS file
 // into a single file.
+//
+// Does this for all 4 combinations of
+//
+// [dev, prod] x [standalone, extension]
+//
 import * as fs from 'fs';
+
+const Targets = ['standalone', 'extension'] as const;
+const Modes = ['dev', 'prod'];
+
+const targetToFilename: Record<(typeof Targets)[number], string> = {
+  standalone: 'page.js',
+  extension: 'vscodeext.js',
+};
 
 const data = fs.readFileSync('./index.html', 'utf8');
 const [before, after] = data.split(
-  '<script type="module" src="./dst/page.js"></script>'
+  '<script type="module" src="./dst/dev/page.js"></script>'
 );
 
-const script = fs.readFileSync('./dst/vscodeext.js', 'utf8');
+Targets.forEach((target) => {
+  Modes.forEach((mode) => {
+    const targetFilename = targetToFilename[target];
+    const script = fs.readFileSync(`./dst/${mode}/${targetFilename}`, 'utf8');
 
-fs.writeFileSync(
-  './dst/merged.html',
-  `${before}
-<script type="module">
-  ${script}
-</script>
-${after}
-`
-);
+    const dir = `./dst/${mode}/${target}/`;
+    fs.mkdirSync(dir, { recursive: true });
+
+    fs.writeFileSync(
+      `./dst/${mode}/${target}/index.html`,
+      `${before}
+    <script type="module">
+      ${script}
+    </script>
+    ${after}
+    `
+    );
+  });
+});
