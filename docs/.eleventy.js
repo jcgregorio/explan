a8ef78e29ab5ae1af0364e39a67a337aaac2ca88
Joe Gregorio <joe@bitworking.org>
2025-05-14 00:04:41 -0400

    Add some intro text and images to site.

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -2,6 +2,8 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     '../dst/prod/standalone/index.html': 'play/index.html',
   });
+  eleventyConfig.addPassthroughCopy('src/images/');
+
   return {
     dir: {
       input: 'src',
